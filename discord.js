3150500db8b2f1ae440cddc8fbf85a9742e0e254
ThomasHoltkamp <thomas.holtkamp@student.graafschapcollege.nl>
2019-03-22 22:55:05 +0100

    added avatar viewer

--- discord.js
+++ discord.js
@@ -8,6 +8,7 @@ client.commands = new Discord.Collection();
 client.commands.set('ping', require('./commands/ping.js'));
 client.commands.set('reload', require('./commands/reload.js'));
 client.commands.set('rip', require('./commands/rip.js'));
+client.commands.set('avatar', require('./commands/avatar.js'));
 
 client.on('ready', () => require('./events/ready.js')(client));
 client.on('message', message => require('./events/message.js')(client, message));
