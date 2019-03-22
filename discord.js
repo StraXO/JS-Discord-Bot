bf3ef986c9db93fdab416dbe2cc61114561f5611
ThomasHoltkamp <thomas.holtkamp@student.graafschapcollege.nl>
2019-03-22 20:59:19 +0100

    stupid mistake

--- discord.js
+++ discord.js
@@ -11,5 +11,5 @@ client.commands.set('rip', require('./commands/rip.js'));
 
 client.on('ready', () => require('./events/ready.js')(client));
 client.on('message', message => require('./events/message.js')(client, message));
-console.log(process.env.client_token);
-client.login(process.env.config_token);
+
+client.login(process.env.client_token);
