536fec5f9976cdce4d39897cf105b729fbc61a75
ThomasHoltkamp <thomas.holtkamp@student.graafschapcollege.nl>
2019-03-23 13:34:54 +0100

    New command ping working

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,6 @@
-exports.run = (client, message, args) => {
-   message.channel.send('Pong!');
+exports.run = async(client, message, args) => {
+  // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+  // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+  const m = await message.channel.send("Ping?");
+  m.edit(`Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
 }
