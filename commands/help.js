c8f49c2781e34ed7167d2b89eb393df496d308a6
StraXO <uitgeopt1@gmail.com>
2019-04-01 18:15:00 +0200

    Change help

--- commands/help.js
+++ commands/help.js
@@ -2,20 +2,24 @@ module.exports.run = async(client, message, args, guildConf) => {
   let prefix = guildConf.prefix;
 
   const embed = {
-  "title": `Useage: ${prefix}(command)`,
-  "description": "For more info visit [Github](https://github.com/StraXO/WeebMaster). ```\nHelp, shows this help screen\navatar\nping\npurge\nrip\nsettings```",
+  "title": `Useage: **${prefix}(command)**`,
+  "description": "For more info visit [Github](https://github.com/StraXO/WeebMaster). ```\nhelp: shows this help screen\navatar\nping\npurge\nrip\nsettings```",
   "url": "https://discordapp.com",
   "color": 13402724,
   "timestamp": "2019-04-01T09:06:00.435Z",
   "footer": {
-    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
-    "text": "footer text"
+    "icon_url": "https://cdn.discordapp.com/avatars/558227965473980416/df9d3d3821b7791a4d86ffee92fd87bf.png?size=2048",
+    "text": "WeebMaster"
   },
   "fields": [
     {
+      "name": "Prefix",
+      "value": "The current prefix is set to: ' + prefix +'"
+    },
+    {
       "name": "🤔",
       "value": "I AM NOT FINISHED..."
-    },
+    }
   ]
 };
 
