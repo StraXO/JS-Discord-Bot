8a74721fe80dc01607e844b7812b925fb72db7a0
StraXO <uitgeopt1@gmail.com>
2019-04-01 11:53:31 +0200

    change ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,8 @@
 exports.run = async(client, message, args) => {
   // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
   // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-  const m = await message.channel.send("Ping?");
-  message.channel.send("Pinging ...") // Placeholder for pinging ...
-  			.then((msg) => { // Resolve promise
-  				msg.edit("Ping: " + (Date.now() - msg.createdTimestamp) + "ms") // Edits message with current timestamp minus timestamp of message
-  			});}
+  message.channel.send("Testing ping...") // Placeholder for pinging ...
+  .then((msg) => { // Resolve promise
+  	msg.edit("Ping: " + (Date.now() - msg.createdTimestamp) + "ms") // Edits message with current timestamp minus timestamp of message
+  });
+}
--- discord.js
+++ discord.js
@@ -16,12 +16,13 @@ client.settings = new Enmap({
 //Commands
 client.commands = new Discord.Collection();
 
+client.commands.set('help', require('./commands/settings.js'));
+client.commands.set('settings', require('./commands/help.js'));
 client.commands.set('ping', require('./commands/ping.js'));
 client.commands.set('reload', require('./commands/reload.js'));
 client.commands.set('rip', require('./commands/rip.js'));
 client.commands.set('avatar', require('./commands/avatar.js'));
 client.commands.set('purge', require('./commands/purge.js'));
-client.commands.set('settings', require('./commands/settings.js'));
 
 client.on('ready', () => require('./events/ready.js')(client, pool));
 client.on('message', message => require('./events/message.js')(client, message, pool));
