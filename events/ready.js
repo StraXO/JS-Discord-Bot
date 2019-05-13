f8a0e2a9000b0087651ccfd35badbce01014d06a
StraXO <uitgeopt1@gmail.com>
2019-05-13 13:18:44 +0200

    Better ready information

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,8 @@
 module.exports = async (client, pool, defaultSettings) => {
+  //Setup bot
   console.log(`[STARTUP] Logged in as: ${client.user.tag}`);
-
+  //Set the bot's activity
   client.user.setActivity(`Currently serving ${client.guilds.size} servers`);
-
-  let totalUsers = 0;
-  let serverList = "";
-  client.guilds.forEach((guild) => {
-    serverList += `[STARTUP] - ${guild.name} (${guild.memberCount}) \n`;
-    totalUsers += guild.memberCount;
-  });
-  console.log(`[STARTUP] Servers: ${client.guilds.size} Users: ${totalUsers} \n${serverList}`);
 
   //Database connection
   pool.connect( async (err, clientDB, done) => {
@@ -21,12 +14,26 @@ module.exports = async (client, pool, defaultSettings) => {
       //disconnent from database on error
       done(err);
     });
-
+    //Get all database information
     let result = await clientDB.query('SELECT * from guilds');
 
+    //Calculate user amount and saves amount of users total
+    let totalUsers = 0;
+    let listServers = [];
+
+    client.guilds.forEach((guild) => {
+      totalUsers += guild.memberCount;
+      listServers.push(`${guild.memberCount}`);
+    });
+    //Log servers and users total
+    console.log(`[STARTUP] Servers: ${client.guilds.size} Users: ${totalUsers}`);
+
+    //Log all servers with corresponding data for debug
+    let i = 0;
     result.rows.forEach((guild) => {
       client.settings.set(guild.id, guild.prefix, "prefix");
-      console.log(`[STARTUP] ${client.guilds.get(guild.id)} (${guild.id}) ${guild.prefix}`);
+      console.log(`[STARTUP] ${client.guilds.get(guild.id)} (${listServers[i]}) (${guild.id}) ${guild.prefix}`);
+      i++;
     });
   });
 }
