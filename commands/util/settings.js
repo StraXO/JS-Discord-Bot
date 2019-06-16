d0d60899a1ac74fb638951eb1e9f61f7c43facd4
StraXO <uitgeopt1@gmail.com>
2019-06-16 22:17:29 +0200

    Beautify settings.js

--- commands/util/settings.js
+++ commands/util/settings.js
@@ -3,29 +3,29 @@ const Discord = require('discord.js')
 exports.run = (client, message, args, guildConf, pool) => {
   if (message.channel.type !== 'text') return message.reply('I can\'t execute that command inside DMs!');
 
-    if(!message.channel.permissionsFor(message.author).has("MANAGE_CHANNELS")) {
-      return message.reply("You don't have the permissions to change the prefix!");
+  if(!message.channel.permissionsFor(message.author).has("MANAGE_CHANNELS")) {
+    return message.reply("You don't have the permissions to change settings!");
+  }
+
+  // This is array destructuring
+  const [prop, ...value] = args;
+
+  if (!prop || prop === "show" || prop === "list" || prop === "current") {
+    //show settings overview
+    let configProps = Object.keys(guildConf).map(prop => {
+      return `${prop}  :  ${guildConf[prop]}\n`;
+    });
+
+    return message.channel.send(`The following are the server's current configuration:
+    \`\`\`${configProps}\`\`\``);
+  } else {
+    // settings
+    let settingFile = client.setting.get(prop);
+
+    if (settingFile) {
+      settingFile.run(client, message, args, guildConf, pool);
     }
-
-    // This is array destructuring
-    const [prop, ...value] = args;
-
-    if (!prop || prop === "show" || prop === "list" || prop === "current") {
-      //show settings overview
-      let configProps = Object.keys(guildConf).map(prop => {
-        return `${prop}  :  ${guildConf[prop]}\n`;
-      });
-      return message.channel.send(`The following are the server's current configuration:
-      \`\`\`${configProps}\`\`\``);
-
-    } else {
-      // settings
-      let settingFile = client.setting.get(prop);
-
-      if (settingFile) {
-        settingFile.run(client, message, args, guildConf, pool);
-      }
-    }
+  }
 }
 
 module.exports.config = {
